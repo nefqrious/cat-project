a67bd93c26646e885066910a1a7f3143be6b8168
nefqrious <sonnyelijahalbano@gmail.com>
2022-09-01 09:38:21 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" _._     _,-'\"\"`-._\n" +
+                "(,-.`._,'(       |\\`-/|\n" +
+                "    `-.-' \\ )-`( , o o)\n" +
+                "          `-    \\`_`\"'-");
+        System.out.println("cat!!!!");
+    }
+}
